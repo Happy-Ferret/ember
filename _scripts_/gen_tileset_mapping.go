0b2bf237524a0989c6274b4bb23ee3b4ae6d96da
mewmew <rnd0x00@gmail.com>
2017-04-29 18:47:34 +0200

    spark: initial commit; add Tristram map
    Create a minimal FLARE mod for Diablo 1.
    
    Resources must be generated from the
    original MPQ of the game.

---
+++ _scripts_/gen_tileset_mapping.go
@@ -0,0 +1,19 @@
+// generate "tilesetdefs/tileset_town.txt"
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("img=images/tilesets/tileset_town.png")
+	fmt.Println()
+	x, y := 0, 0
+	for id := 1; id <= 1258; id++ {
+		fmt.Printf("tile=%d,%d,%d,64,256,32,240\n", id, x*64, y*256)
+		x++
+		if x >= 79 {
+			x = 0
+			y++
+		}
+	}
+}
