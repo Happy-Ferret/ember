95000021006f18b1cd0f39265c6c02484a1de642
mewmew <rnd0x00@gmail.com>
2017-04-29 20:01:29 +0200

    spark: add collisions to town map

--- _scripts_/gen_tilesetdefs.go
+++ _scripts_/gen_tilesetdefs.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println()
 	x, y := 0, 0
 	for id := 1; id <= 1258; id++ {
-		fmt.Printf("tile=%d,%d,%d,64,256,32,240\n", id, x*64, y*256)
+		fmt.Printf("tile=%d,%d,%d,64,256,32,240\n", id+100, x*64, y*256)
 		x++
 		if x >= 79 {
 			x = 0
