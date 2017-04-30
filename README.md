e0136e95d0f5c6540414b0bc4dd8d2cf44d77521
mewmew <rnd0x00@gmail.com>
2017-04-30 04:08:09 +0200

    spark: add README and opensourceami script

---
+++ README.md
@@ -0,0 +1,72 @@
+# Spark
+
+The aim of this project is to get Diablo 1 running on the FLARE game engine.
+
+## Current progress
+
+* [x] convert the map of Tristram to TMX format.
+
+![Tristram](http://oi65.tinypic.com/153lbiu.jpg)
+
+## Installation
+
+Note, this game requires an original copy of `diabdat.mpq`.
+
+### Install FLARE engine
+
+```bash
+# Install dependencies of FLARE.
+pacman -S sdl2 sdl2_image sdl2_mixer sdl2_ttf
+
+# Clone FLARE engine and game assets.
+git clone https://github.com/clintbellanger/flare-engine
+git clone https://github.com/clintbellanger/flare-game
+
+# Clone the Spark game
+git clone https://github.com/sanctuary/spark
+
+# Add symlinks to default, fantasycore and alpha_demo mods.
+cd spark/mods
+ln -s ../../flare-engine/mods/default
+ln -s ../../flare-game/mods/fantasycore
+ln -s ../../flare-game/mods/alpha_demo
+cd ..
+ln -s ../flare-engine/flare
+```
+
+### Generate game assets
+
+```bash
+# Get assets conversion tools.
+go get github.com/mewrnd/blizzconv/...
+go get github.com/sanctuary/spark/_scripts_/opensourceami
+
+# Create "spark/_assets_" directory.
+mkdir _assets_
+cd _assets_
+
+# Extract diabdat.mpq to the "_assets_/mpqdump" directory.
+#
+# Note, you may use Ladislav Zezula's MPQ Editor to extract the contents of diabdat.mpq.
+
+# Extract game assets. Takes roughly 5 minutes.
+opensourceami -o extract_assets.sh
+./extract_assets.sh
+cd ..
+```
+
+### Run the game
+
+```bash
+# Standing in the spark directory, run `./flare` and navigate to the
+# Configuration menu, the Mods tab, and enable the spark mod.
+#
+# Now you may launch Spark by pressing Play Game.
+./flare
+```
+
+## Public domain
+
+The source code and any original content of this repository is hereby released into the [public domain].
+
+[public domain]: https://creativecommons.org/publicdomain/zero/1.0/
---
+++ _scripts_/opensourceami/main.go
@@ -0,0 +1,93 @@
+// Tribute to OpenBSD: https://www.openbsd.org/lyrics.html#41
+
+// Open Source-ami converts the original Diablo 1 game assets into the file
+// formats used by Spark.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/mewkiz/pkg/goutil"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	// Parse command line arguments.
+	var (
+		// output specifies the output path.
+		output string
+	)
+	flag.StringVar(&output, "o", "", "output path")
+	flag.Parse()
+	w := os.Stdout
+	if len(output) > 0 {
+		f, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			log.Fatalf("unable to create %q; %v", output, err)
+		}
+		defer f.Close()
+		w = f
+	}
+
+	// Generate game assets script.
+	if err := opensourceami(w); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+// opensourceami generates a script for converting the original Diablo 1 game
+// assets into the file formats used by Spark.
+func opensourceami(w io.Writer) error {
+	dir, err := goutil.SrcDir("github.com/mewrnd/blizzconv")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if _, err := fmt.Fprintf(w, script[1:], dir, dir, dir, dir); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+const script = `
+#!/bin/bash
+
+# Locate extracted diabdat.mpq
+if [ ! -f "mpqdump/levels/towndata/town.cel" ]; then
+	echo "Unable to locate \"mpqdump\" directory containing the contents of diabdat.mpq"
+	echo ""
+	echo "   Please extract diabdat.mpq using Ladislav Zezula's MPQ Editor [1]."
+	echo ""
+	echo "   [1]: http://www.zezula.net/en/mpq/download.html"
+	exit 1
+fi
+
+# Add symlinks to cel.ini, cl2.ini, dun.ini and mpq.ini.
+ln -s %s/images/imgconf/cel.ini
+ln -s %s/images/imgconf/cl2.ini
+ln -s %s/configs/dunconf/dun.ini
+ln -s %s/mpq/mpq.ini
+
+# Convert CEL, CL2, MIN, TIL and DUN files to PNG images.
+echo "*.cel"
+time img_dump -a -imgini=cel.ini
+#echo "*.cl2"
+#time img_dump -a -imgini=cl2.ini
+echo "*.min"
+time min_dump town.min l1.min l2.min l3.min l4.min
+#echo "*.til"
+#time til_dump town.til l1.til l2.til l3.til l4.til
+#echo "*.dun"
+#time dun_dump -a
+
+# Generate the tileset for Tristram.
+mkdir -p ../mods/spark/images/tilesets
+montage _dump_/_pillars_/town/pillar_*.png -background none -tile x16 -geometry 64x256 ../mods/spark/images/tilesets/tileset_town.png
+
+# Generate the music for Tristram.
+mkdir -p ../mods/spark/music
+ffmpeg -i mpqdump/music/dtowne.wav ../mods/spark/music/town.ogg
+`
